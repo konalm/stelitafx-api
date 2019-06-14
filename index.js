52f264eced6f4286b22442bab7d16da0fe4957e1
connor <connor@codegood.co>
2019-06-15 00:00:07 +0100

    live release test

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express()
 const port = 8000
 
 
-app.get('/hello', (req, res) => res.send('Hello from Stelita FX api inside docker container'))
+app.get('/hello', (req, res) => res.send('Hello from Stelita FX api inside docker container LIVE'))
 
 app.listen(port, () => console.log(`Stelita FX API listening in port ${port}`))
